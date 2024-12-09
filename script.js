c541ac26a11a5f1720c47d3fa7a5739e0d50c8fc
Ibinthomas <ibinthomas858@gmail.com>
2024-12-09 12:47:44 +0530

    java

---
+++ script.js
@@ -0,0 +1,23 @@
+// alert("welcome to all")
+
+// a="welcome back"
+// console.log("hello",a);
+
+// var a="hello"
+// a=20
+
+// console.log(a);
+
+// let a=20
+// console.log(a);
+
+let a={name:'ibin',age:21}
+let b=false
+let c=[1,2]
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+console.log(a)
+console.log(b)
+console.log(c)
