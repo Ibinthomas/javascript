5b5732a98e6aa6317d6dcf7d0b21815714b6557a
Ibinthomas <ibinthomas858@gmail.com>
2024-12-10 13:30:26 +0530

    add to div

--- script.js
+++ script.js
@@ -72,3 +72,64 @@ function mode(){
     let h2=document.getElementById("dis")
     h2.innerHTML=no1%no2
 }
+
+function equal(){
+    let a=parseInt(document.getElementById("a").value)
+    let b=parseInt(document.getElementById("b").value)
+    let h2=document.getElementById("dis")
+    
+    if (a==b){
+        h2.innerHTML='eql'
+    }
+    else if(a!=b)
+        h2.innerHTML='not eql'
+}
+
+function check(){
+    let city=document.getElementById("city").value
+    let h2=document.getElementById("dis")
+    if (city=='delhi'){
+        h2.innerHTML='mounment red fort'
+    }
+    else if(city=='kerala'){
+        h2.innerHTML='alappuzha'
+    }
+
+    else if(city=='goa'){
+        h2.innerHTML='beatch'
+    }
+
+}
+
+function days(){
+    let day=document.getElementById("day").value
+    let h2=document.getElementById("dis")
+    if (day=='1'){
+        h2.innerHTML='sunday'
+    }
+    else if(day=='2'){
+        h2.innerHTML='monday'
+    }
+
+    else if(day=='3'){
+        h2.innerHTML='tuesday'
+    }
+
+    else if(day=='4'){
+        h2.innerHTML='wednesday'
+    }
+
+    else if(day=='5'){
+        h2.innerHTML='thursday'
+    }
+
+    else if(day=='6'){
+        h2.innerHTML='friday'
+    }
+
+    else if(day=='7'){
+        h2.innerHTML='saturday'
+    }
+
+}
+
