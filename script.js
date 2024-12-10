5fb6b6f831f33fa5daa3b13ab7d35e99bb010f07
Ibinthomas <ibinthomas858@gmail.com>
2024-12-10 12:39:23 +0530

    add to div

--- script.js
+++ script.js
@@ -21,3 +21,54 @@ console.log(typeof c);
 console.log(a)
 console.log(b)
 console.log(c)
+
+
+// ___________________________________________
+
+function sample(){
+    // let result=document.getElementById('name')
+    //     console.log(result.value);
+    //     document.getElementById("display").innerHTML=result.value
+
+    // let result=document.getElementById('name').value
+    // console.log(result);
+    // let h2=document.getElementById('display')
+    // h2.innerHTML=result
+    
+
+}
+
+function add(){
+    let no1=parseInt(document.getElementById("no1").value)
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("dis")
+    h2.innerHTML=no1+no2
+}
+
+function sub(){
+    let no1=parseInt(document.getElementById("no1").value)
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("dis")
+    h2.innerHTML=no1-no2
+}
+
+function mult(){
+    let no1=parseInt(document.getElementById("no1").value)
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("dis")
+    h2.innerHTML=no1*no2
+}
+
+function div(){
+    let no1=parseInt(document.getElementById("no1").value)
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("dis")
+    h2.innerHTML=no1/no2
+}
+
+function mode(){
+    let no1=parseInt(document.getElementById("no1").value)
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("dis")
+    h2.innerHTML=no1%no2
+}
