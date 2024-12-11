73f7007fc2f946314fa7d8e0ee5e6b2af3ddf3b5
Ibinthomas <ibinthomas858@gmail.com>
2024-12-11 13:31:35 +0530

    loop

---
+++ loop.js
@@ -0,0 +1,51 @@
+// function loop(){
+//     let n=document.getElementById('n').value
+
+//     for (i=1;i<=n;i++){
+//         console.log(i);
+
+//     }
+//     let i=1
+//     while(i<=n){
+//         console.log(i);
+//         i++
+//     }
+
+//     let i=1
+//     do{
+//         console.log(i);
+//         i++
+//     }
+//     while(i<=n)
+    
+
+// }
+
+// function number(){
+//     let start=document.getElementById('start').value
+//     let end=document.getElementById('end').value
+
+//     for (let i=start;i<=end;i++){
+//         if (i%2==0){
+//             console.log(i);
+//         }
+//     }
+
+// }
+
+// function mult(){
+//     let num=parseInt(document.getElementById('no').value)
+//     for (let i=1;i<=10;i++)
+//         console.log(i,'*',num,'=',i*num);
+// }
+
+
+function sum(){
+    let start=document.getElementById('start').value
+    let end=document.getElementById('end').value
+    let sum=0
+    
+    
+
+}
+    
